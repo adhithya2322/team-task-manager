165c8b705d596b9e59ab4860f897611777435037
Adhithya <adhithyagunti2322@gmail.com>
2026-05-10 19:41:21 +0530

    connected frontend to railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://team-task-manager-production-997b.up.railway.app/api",
 });
 
 export default API;
