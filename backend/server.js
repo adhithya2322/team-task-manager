f5264aad62a3d6c76e4e49db84c354034b05b337
Adhithya <adhithyagunti2322@gmail.com>
2026-05-11 12:32:16 +0530

    fixed railway backend

--- backend/server.js
+++ backend/server.js
@@ -3,39 +3,34 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-// DATABASE CONNECTION
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running");
+    });
   })
   .catch((err) => {
     console.log(err);
   });
-
-// ROUTES
-app.use("/api/users", userRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
-
-// TEST ROUTE
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-// SERVER
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
