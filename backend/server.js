15442987de05a76703d25d93c5f86f92b6b94617
Adhithya <adhithyagunti2322@gmail.com>
2026-05-11 12:40:50 +0530

    fixed railway port

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend running successfully");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
@@ -20,10 +24,12 @@ app.use("/api/tasks", taskRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected");
+    console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Server running");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
