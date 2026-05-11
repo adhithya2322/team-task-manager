89dfc61102fccec91f2272af7c4374575991d83c
Adhithya <adhithyagunti2322@gmail.com>
2026-05-11 12:36:36 +0530

    fixed server config

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -16,16 +16,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/users", userRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Backend Running");
-});
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB Connected");
+    console.log("MongoDB connected");
 
     app.listen(process.env.PORT || 5000, () => {
       console.log("Server running");
